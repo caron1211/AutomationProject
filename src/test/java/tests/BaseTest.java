32d5b6f8c3e3e2d37d716b75095cf936ee776f2d
biranvaron <biraniv@gmail.com>
2020-09-08 10:11:05 +0300

    update

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,13 +1,19 @@
 package tests;
 
+import static io.restassured.RestAssured.*;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.ITestContext;
 import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.testng.ITestContext;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 public class BaseTest {
 	WebDriver driver;
@@ -20,8 +26,24 @@ public class BaseTest {
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		testContext.setAttribute("WebDriver", this.driver);
 		driver.get("https://www.saucedemo.com/");
-
 	}
+
+	@Test
+	public void getResponseTime() {
+		RequestSpecification requestSpec = RestAssured.given();
+		Response responseTime = requestSpec.get("https://www.saucedemo.com/");
+
+		// Print response time
+		System.out.println("Response Time : " + responseTime.getTime());
+	}
+
+	@Test
+	public void getStatusCode() {
+		RequestSpecification httpRequest = RestAssured.given();
+		Response responseStatusCode = httpRequest.get("https://www.saucedemo.com/");
+		int statusCode = responseStatusCode.getStatusCode();
+		Assert.assertEquals(statusCode, 200, "Correct status code returned");
+	 }
 
 	@AfterClass
 	public void tearDown() {
